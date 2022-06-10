6e205f508ed6f4fd47d31abd4251123b9c367cf1
hamic <iayhytf2011+2015>
2022-06-10 11:36:15 +0200

    Open Food Fact Algo : - Create class for category
    By Blempy - 10/06/2022 - 11h35

--- off_algo.py
+++ off_algo.py
@@ -33,3 +33,31 @@ class Category:
                 self.dict_category[self.id_list[i]] = self.url_list[i]
                 i += 1
         return self.dict_category
+
+
+"""
+# Produits dans la categorie : "count"
+# Numero de la page : "page"
+# Produits par page : "page_size"
+# Produits : "products"
+
+Elements à recuperer dans "products" : 
+- image_url
+- nutriscore_grade / - nutrition_grade_fr
+- product_name / - product_name_fr
+"""
+
+
+# cat_object = Category()
+# cat_object.get_random_id()
+# cat_object.get_url_from_random_id()
+# cat_object.make_category_dict()
+#
+# product_url_list = []
+# for i in cat_object.dict_category.values():
+#     product_url_list.append(i)
+
+class Product(Category):
+    def __init__(self):
+        self.request_product = requests.get(Product.dict_category.values())
+        self.json_product = self.request_product.json()
